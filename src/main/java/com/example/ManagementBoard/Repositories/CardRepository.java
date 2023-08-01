161409f5f779c7991a43c60ac0ef3a708e1d8d07
ayou6 <ayouxb.abbas@gmail.com>
2023-08-01 14:57:27 +0400

    adding classes

---
+++ src/main/java/com/example/ManagementBoard/Repositories/CardRepository.java
@@ -0,0 +1,8 @@
+package com.example.ManagementBoard.Repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+@Repository
+public class CardRepository {
+    //extends JpaRepository<Card, Long>
+}
