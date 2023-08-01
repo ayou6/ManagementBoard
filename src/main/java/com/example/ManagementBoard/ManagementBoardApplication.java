6c5454d23f73086f3a2dc41dd5494f3b7b868515
ayou6 <ayouxb.abbas@gmail.com>
2023-08-01 12:08:12 +0400

    1st commit

---
+++ src/main/java/com/example/ManagementBoard/ManagementBoardApplication.java
@@ -0,0 +1,13 @@
+package com.example.ManagementBoard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagementBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagementBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ManagementBoard/ManagementBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ManagementBoard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
